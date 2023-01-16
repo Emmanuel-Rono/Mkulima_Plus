c7087b77c76434c59807de8e511036c38749509f
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-01-16 15:50:52 +0300

    Innitial Commit

---
+++ app/src/main/java/com/example/mkulima_plus/view_Model/CropsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mkulima_plus.view_Model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mkulima_plus.R
+
+class CropsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crops)
+    }
+}
---
+++ app/src/main/java/com/example/mkulima_plus/view_Model/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mkulima_plus.view_Model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mkulima_plus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
