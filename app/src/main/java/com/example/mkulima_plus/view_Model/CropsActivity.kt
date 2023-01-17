773f56e4c1a9dcb702d9400cf1a44d7fe1d038b7
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-01-17 11:29:41 +0300

    Proceeding

--- app/src/main/java/com/example/mkulima_plus/view_Model/CropsActivity.kt
+++ app/src/main/java/com/example/mkulima_plus/view_Model/CropsActivity.kt
@@ -1,12 +1,42 @@
 package com.example.mkulima_plus.view_Model
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.text.TextUtils.replace
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.fragment.app.*
 import com.example.mkulima_plus.R
 
 class CropsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crops)
+        //get the views
+        val FragmentContainer = findViewById<FragmentContainerView>(R.id.fragmentContainerView)
+        val ToolBar = findViewById<Toolbar>(R.id.ToolBar)
+        val About_Fragment = About()
+        val Atack_fragment = Attacks()
+        val Tips_Fragment = Tips()
+
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when (item.itemId)
+                R.id.Attacks ->
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView, About_Fragment)
+                commit()
+            }
+            return super.onOptionsItemSelected(item)
+        }
     }
+
+
+
+
+        }
+
 }
