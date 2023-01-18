e45599530c92712e01e89c683c40b2d48b2901e9
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-01-18 16:52:40 +0300

    Proceeding

--- app/src/main/java/com/example/mkulima_plus/view_Model/CropsActivity.kt
+++ app/src/main/java/com/example/mkulima_plus/view_Model/CropsActivity.kt
@@ -7,31 +7,38 @@ import android.text.TextUtils
 import android.text.TextUtils.replace
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.fragment.app.*
 import com.example.mkulima_plus.R
-
 class CropsActivity : AppCompatActivity() {
-
-    val FragmentContainer = findViewById<FragmentContainerView>(R.id.fragmentContainerView)
-    val ToolBar = findViewById<Toolbar>(R.id.ToolBar)
-    val About_Fragment = About()
     val Atack_fragment = Attacks()
-    val Tips_Fragment = Tips()
+    //val About_fragment=About()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crops)
-        //get the views
-
+       //if (savedInstanceState == null) {
+           // supportFragmentManager.commit {
+              //  setReorderingAllowed(true)
+              //  add<About>(R.id.fragmentContainerView)
+           // }
+        //}
+        //Access the Recyclerview
     }
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            if(item.itemId == R.id.Attacks)
-                supportFragmentManager.beginTransaction().apply {
-                    replace(R.id.fragmentContainerView, About_Fragment)
-                    commit()
-                }
-            return super.onOptionsItemSelected(item)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.tool_bar_menu,menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+    {
+        when(item.itemId){R.id.fragment_attacks->
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,Atack_fragment)
         }
+        Toast.makeText(this,"Attack clicked",Toast.LENGTH_LONG).show()
+        return super.onOptionsItemSelected(item)
     }
+    }
+
+
 
 
--- app/src/main/java/com/example/mkulima_plus/view_Model/MainActivity.kt
+++ app/src/main/java/com/example/mkulima_plus/view_Model/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.mkulima_plus.view_Model
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import com.example.mkulima_plus.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val test: Button =findViewById(R.id.button)
+
+            test.setOnClickListener {
+                val intent=Intent(this,CropsActivity::class.java)
+                startActivity(intent)
+                Log.d(TAG, "Error here")
+
+            }
+        }
+
+
     }
-}
