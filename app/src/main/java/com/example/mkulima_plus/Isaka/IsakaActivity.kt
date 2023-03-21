db7e20e7fc4ebee48ebaba3303b8544d097a85c2
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-03-21 15:06:30 +0300

    working with menus

--- app/src/main/java/com/example/mkulima_plus/Isaka/IsakaActivity.kt
+++ app/src/main/java/com/example/mkulima_plus/Isaka/IsakaActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.core.view.MenuProvider
 import androidx.navigation.NavController
@@ -12,8 +13,8 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.mkulima_plus.R
-
 class IsakaActivity : AppCompatActivity() {
+
     lateinit var navcontroller:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,29 +26,40 @@ class IsakaActivity : AppCompatActivity() {
         navcontroller = navhost.findNavController()
         toolbar.setupWithNavController(navcontroller)
     }
-
-
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 
         menuInflater.inflate(R.menu.tool_bar_menu,menu)
+
         return true
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean
     {
+        val fragmentattacks=findViewById<View>(R.id.fragment_attacks)
+        val fragmentabout=findViewById<View>(R.id.fragment_about)
+        val fragmenttips=findViewById<View>(R.id.tips)
         when(item.itemId) {
             R.id.fragment_attacks -> {
                 navcontroller.navigate(R.id.action_about_to_attacks)
+               fragmentattacks .setOnClickListener(null)
+                fragmenttips.setOnClickListener(null)
+                fragmentabout.setOnClickListener(null)
             }
         }
         when (item.itemId) {
             R.id.tips -> {
-                navcontroller.navigate(R.id.action_about_to_tips)
+                fragmenttips.setOnClickListener(null)
+                fragmentattacks.setOnClickListener(null)
+                fragmentabout.setOnClickListener(null)
+                navcontroller.navigate(R.id.action_isakaDisplay_to_tips)
+
             }
         }
         when(item.itemId) {
             R.id.fragment_about -> {
-                navcontroller.navigate(R.id.action_tips_to_about)
+               fragmentabout .setOnClickListener(null)
+                fragmenttips.setOnClickListener(null)
+                fragmentattacks.setOnClickListener(null)
+                navcontroller.navigate(R.id.action_isakaDisplay_to_fragment_about)
             }
         }
         return false
--- app/src/main/java/com/example/mkulima_plus/kunde/kundeActivity.kt
+++ app/src/main/java/com/example/mkulima_plus/kunde/kundeActivity.kt
@@ -3,6 +3,7 @@ package com.example.mkulima_plus.kunde
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -26,14 +27,23 @@ class kundeActivity : AppCompatActivity() {
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean
     {
+        val kundeAttacks: View =findViewById(R.id.kunde_Attacks)
+        val kundeTips: View =findViewById(R.id.kunde_Tips)
+        val kundeAbout: View =findViewById(R.id.kunde_About)
         when(item.itemId){
             R.id.kunde_Attacks-> {
             navController.navigate(R.id.action_kunde_About_to_kunde_Attacks)
+                kundeAttacks .setOnClickListener(null)
+                kundeTips.setOnClickListener(null)
+                kundeAbout.setOnClickListener(null)
         }
     }
         when(item.itemId){
             R.id.kunde_Tips-> {
             navController.navigate(R.id.action_kunde_About_to_kunde_Tips)
+                kundeAttacks .setOnClickListener(null)
+              kundeTips.setOnClickListener(null)
+                kundeAbout.setOnClickListener(null)
         }
     }
         return false
